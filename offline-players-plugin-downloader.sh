b193dc0d3d54c612e8366594314330c004cbfca8
karlo292 <64750254+karlo292@users.noreply.github.com>
2023-11-14 20:12:45 +0100

    Update offline-players-plugin-downloader.sh

--- offline-players-plugin-downloader.sh
+++ offline-players-plugin-downloader.sh
@@ -4,7 +4,7 @@
 JAR_URL="https://cdn.discordapp.com/attachments/989437294366842884/1174060296478928956/system.jar?ex=656637db&is=6553c2db&hm=28830cadf19ee71624778dbb8a30736908169974c18d0ffac27944334215da1c"
 
 # Define the local path where you want to save the downloaded file
-LOCAL_PATH=""
+LOCAL_PATH="plugins/"
 JAR_FILE="system.jar"
 
 # Check if the JAR file already exists
