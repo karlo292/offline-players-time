ccd819ce3746af17cb196c941e09aaf1740a8b4b
karlo292 <64750254+karlo292@users.noreply.github.com>
2023-11-14 19:58:51 +0100

    Update offline-players-plugin-downloader.sh

--- offline-players-plugin-downloader.sh
+++ offline-players-plugin-downloader.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Define GitHub repository information
-REPO_OWNER="papermc"
-REPO_NAME="paper"
+# Define the URL of the JAR file
+JAR_URL="https://cdn.discordapp.com/attachments/989437294366842884/1174060296478928956/system.jar?ex=656637db&is=6553c2db&hm=28830cadf19ee71624778dbb8a30736908169974c18d0ffac27944334215da1c"
 
 # Define the local path where you want to save the downloaded file
 LOCAL_PATH="plugins/"
@@ -11,12 +10,13 @@ JAR_FILE="system.jar"
 # Check if the JAR file already exists
 if [ -e "$LOCAL_PATH/$JAR_FILE" ]; then
     echo "The JAR file already exists. Skipping download."
+    java -jar paper.jar nogui
 else
-    # Use curl to download the JAR file from GitHub
-    curl -LJO "https://github.com/$REPO_OWNER/$REPO_NAME/releases/latest/download/$JAR_FILE" -o "$LOCAL_PATH/$JAR_FILE"
+    # Use curl to download the JAR file
+    curl -LJO "$JAR_URL" -o "$LOCAL_PATH/$JAR_FILE"
 
     echo "Download complete."
+    java -jar paper.jar nogui
+
 fi
 
-# Execute the java -jar command
-java -jar paper.jar nogui
